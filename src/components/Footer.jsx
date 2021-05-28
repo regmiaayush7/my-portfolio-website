9c801594efbb6d5a4a60fbfcaeecf9b2a6a8e445
Aayush Regmi <regmiaayush7@gmail.com>
2021-05-28 18:02:03 +0545

    Basic layout completed and also broken the page down into components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+    return (     
+            <footer className="w3-container w3-teal w3-center w3-margin-top">
+            <p>Find me on social media.</p>
+            <i className="fa fa-facebook-official w3-hover-opacity w3-margin"></i>
+            <i className="fa fa-instagram w3-hover-opacity w3-margin"></i>
+            <i className="fa fa-snapchat w3-hover-opacity w3-margin"></i>
+            <i className="fa fa-pinterest-p w3-hover-opacity w3-margin"></i>
+            <i className="fa fa-twitter w3-hover-opacity w3-margin"></i>
+            <i className="fa fa-linkedin w3-hover-opacity w3-margin"></i>
+            <p>Powered by Aayush</p>
+            </footer>
+   
+    )
+}
+
+export default Footer;
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Footer from './Footer'
+import Profile from './Profile'
+import Work from './Work'
+import Education from './Education'
+const Home = () => {
+    return (
+        <div>   
+        <div className ="w3-content w3-margin-top" style={{maxWidth: '1400px'}}>
+            <div className="w3-row-padding">
+                <div className= "w3-third">
+                <Profile />
+                </div>
+
+                <div className="w3-twothird">
+                    <Work />
+                    <Education />
+                </div>
+             </div>        
+         </div>
+        <Footer />                 
+        </div>
+    )
+}
+
+export default Home
